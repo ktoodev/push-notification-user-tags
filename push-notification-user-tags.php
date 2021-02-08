15b1a05c3f3d8dcdc3df5e2f7774ffc13b8a6a97
David Purdy <davidpurdy@gmail.com>
2021-02-08 14:34:21 -0700

    Added subtree for plugin: Push Notification User Tags

---
+++ push-notification-user-tags.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Push Notification User Tags
+ * Description: Add data tags for users to target pushes when used with the OneSignal plugin.
+ * Version: 0.1
+ * Author: David Purdy, KTOO
+ * Author URI: https://www.ktoo.org
+ *
+ */
